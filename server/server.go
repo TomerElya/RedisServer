6d9d6f3d7fd2ff6240890ae43a64f205a5f111e2
TomerElya <TomerElya@users.noreply.github.com>
2020-05-10 14:58:38 +0300

    server.go is implemented with net.listen instead of a http server since it works on fourth layer

--- server/server.go
+++ server/server.go
@@ -3,32 +3,43 @@ package server
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
-	"net/http"
+	"net"
 )
 
 type Server struct {
-	address string
-	port    int
-	mux     *http.ServeMux
+	address  string
+	port     int
+	listener net.Listener
+	store    map[interface{}]interface{}
 }
 
 func CreateServer(address string, port int) Server {
-	return Server{address: address, port: port}
+	return Server{address: address, port: port, store: map[interface{}]interface{}{}}
 }
 
-func (s *Server) Start() bool {
+func (s *Server) StartAndListen() bool {
 	address := fmt.Sprintf("%s:%d", s.address, s.port)
-	s.createMuxHandler()
-	err := http.ListenAndServe(address, nil)
+	listener, err := net.Listen("tcp", address)
+	s.listener = listener
 	if err != nil {
-		log.WithError(err).WithField("address", address)
+		log.WithError(err).WithField("address", address).Error("listener failed to start")
 		return false
 	}
-	log.WithField("address", address).Info("http server successfully started")
+	log.WithField("address", address).Info("listener successfully started")
+	go s.listen()
 	return true
 }
 
-func (s *Server) createMuxHandler() {
-	s.mux = http.NewServeMux()
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.WithError(err).Error("failed receiving new connection")
+		}
+		go s.handleConnection(conn)
+	}
+}
 
+func (s *Server) handleConnection(conn net.Conn) {
+	log.WithField("address", conn.RemoteAddr()).Info("new connection established")
 }
