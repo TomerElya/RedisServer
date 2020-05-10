2026c3a914f029e37b9525498ecb2dea6b713870
TomerElya <TomerElya@users.noreply.github.com>
2020-05-10 14:00:40 +0300

    added logrus and implemented start function for server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type Server struct {
+	address string
+	port    int
+}
+
+func (s *Server) Start() bool {
+	address := fmt.Sprintf("%s:%d", s.address, s.port)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		log.WithError(err).WithField("address", address)
+		return false
+	}
+	return true
+}
