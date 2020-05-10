5c66dc7dcbb7c218ee745ca9de8a107e7421cc49
TomerElya <TomerElya@users.noreply.github.com>
2020-05-10 14:12:34 +0300

    now using mux handler for http server in handlers.go file

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/tomer/RedisServer/server"
+	"os"
+)
+
 func main() {
+	log.SetOutput(os.Stdout)
+	s := server.CreateServer("localhost", 3000)
+	s.Start()
 
 }
--- server/server.go
+++ server/server.go
@@ -9,14 +9,26 @@ import (
 type Server struct {
 	address string
 	port    int
+	mux     *http.ServeMux
+}
+
+func CreateServer(address string, port int) Server {
+	return Server{address: address, port: port}
 }
 
 func (s *Server) Start() bool {
 	address := fmt.Sprintf("%s:%d", s.address, s.port)
+	s.createMuxHandler()
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.WithError(err).WithField("address", address)
 		return false
 	}
+	log.WithField("address", address).Info("http server successfully started")
 	return true
+}
+
+func (s *Server) createMuxHandler() {
+	s.mux = http.NewServeMux()
+
 }
