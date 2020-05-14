e6c1f7414a042543f2e4966c10f835e7f1ba91b7
TomerElya <TomerElya@users.noreply.github.com>
2020-05-14 16:33:29 +0300

    removed pprof code

--- main.go
+++ main.go
@@ -3,26 +3,11 @@ package main
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/tomer/RedisServer/server"
-	"io"
 	"os"
-	"runtime/pprof"
 )
 
 func main() {
-	err := pprof.StartCPUProfile(createProfilingFile())
-	if err != nil {
-		panic(err)
-	}
 	log.SetOutput(os.Stdout)
 	s := server.CreateServer("localhost", 3000)
 	s.StartAndListen()
-	pprof.StopCPUProfile()
-}
-
-func createProfilingFile() io.Writer {
-	file, err := os.Create("./profile.pprof")
-	if err != nil {
-		panic(err)
-	}
-	return file
 }
